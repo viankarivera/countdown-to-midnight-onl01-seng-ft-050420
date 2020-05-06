8615d59f02e4668cd1a2850b92c65e2e713187b9
Vianka Rivera <viankalrivera@gmail.com>
2020-05-06 14:19:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(integer)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(integer)
+  while integer > 0 
+    puts "#{integer} SECOND(S)!"
+    integer -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end
+  
